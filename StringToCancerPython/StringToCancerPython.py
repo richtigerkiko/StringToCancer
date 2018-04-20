6df8bc2ddf6161f8c65967116ddb1fd60cd8ad6f
kafka <elkikos@gmail.com>
2018-04-20 21:06:53 +0200

    better readability

--- StringToCancerPython/StringToCancerPython.py
+++ StringToCancerPython/StringToCancerPython.py
@@ -1,18 +1,15 @@
 def StringToCancer( notCancerYet ):
-
     i = 0
     cancerString = ""
-
-    for char in notCancerYet :
-        if char == " " :
+    for c in notCancerYet :
+        if c == " " :
             i+=1
         if i % 2 == 0 :
-            cancerString = cancerString + char.upper()
+            cancerString = cancerString + c.upper()
         else:
-            cancerString = cancerString + char.lower()
+            cancerString = cancerString + c.lower()
         i+=1
-
     return cancerString;
 
-print(StringToCancer (input("Insert uncancerous string: ")))
+print(StringToCancer(input("Insert uncancerous string: ")))
 
