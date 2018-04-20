78045b7ffe830ddbf6ee57f4b9ebf54534362168
kafka <elkikos@gmail.com>
2018-04-20 22:02:03 +0200

    modulate function

--- StringToCancerFSharpNetCore/Program.fs
+++ StringToCancerFSharpNetCore/Program.fs
@@ -1,19 +1,9 @@
 ﻿open System
 
-let StringToCancer notCancerYet =
-    let mutable cancerString = ""
-    let mutable i = ref 0
-    for c in notCancerYet do
-        if Char.IsWhiteSpace(c) then incr i
-        if i.Value % 2 = 0 then cancerString <- cancerString + c.ToString().ToUpper()
-        else cancerString <- cancerString + c.ToString().ToLower()
-        incr i
-    cancerString
-
 [<EntryPoint>]
 let main argv =
     printfn "Insert uncancerous string:"
-    Console.ReadLine() |> StringToCancer |> printfn "%s" 
+    Console.ReadLine() |> CancerString.StringToCancer.create |> printfn "%s" 
     printfn "Press <ENTER> to close"
     System.Console.ReadLine() |> ignore
     0 
---
+++ StringToCancerFSharpNetCore/StringToCancerFunc.fs
@@ -0,0 +1,14 @@
+﻿namespace CancerString
+open System
+
+module StringToCancer = 
+    type T = {NotCancerYet:string} 
+    let create notCancerYet =
+        let mutable cancerString = ""
+        let mutable i = ref 0
+        for c in notCancerYet do
+            if Char.IsWhiteSpace(c) then incr i
+            if i.Value % 2 = 0 then cancerString <- cancerString + c.ToString().ToUpper()
+            else cancerString <- cancerString + c.ToString().ToLower()
+            incr i
+        cancerString
