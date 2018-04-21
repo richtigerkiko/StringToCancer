a46a416ffe14d346ffdb252926d263fdc97a5955
kafka <elkikos@gmail.com>
2018-04-21 13:42:45 +0200

    Add csharp

---
+++ StringToCancerCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringToCancerCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Insert uncancerous string:");
+                Console.WriteLine(StringToCancerFunc.Create(Console.ReadLine()));
+            }
+        }
+    }
+}
---
+++ StringToCancerCSharp/StringToCancerFunc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringToCancerCSharp
+{
+    class StringToCancerFunc
+    {
+        public static string Create(string notCancerYet)
+        {
+            int i = 0;
+            string cancerString = null;
+            foreach (char c in notCancerYet)
+            {
+                if (char.IsWhiteSpace(c)) i++;
+                if (i % 2 == 0) cancerString += c.ToString().ToUpper();
+                else cancerString += c.ToString().ToLower();
+            }
+            return cancerString;
+        }
+    }
+}
